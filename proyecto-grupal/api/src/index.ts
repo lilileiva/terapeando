91eff57ef156d51dfe7e42a3e7badf66b2ea0b44
sergio17 <sergiosalgado624@gmail.com>
2022-06-06 17:55:55 -0500

    test

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/?retryWrites=true&w=majority')
-   console.log('database is connected to', db.connection.db.databaseName)
+   console.log('database is connected to', db.connection.db)
 }
 
 connectDB()
