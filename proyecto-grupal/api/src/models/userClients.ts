a94160a42bd10d8965b5d4c4aa168f5a463e4756
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-06 17:48:18 -0500

    mongo model started

---
+++ proyecto-grupal/api/src/models/userClients.ts
@@ -0,0 +1,19 @@
+import {prop, getModelForClass} from '@typegoose/typegoose'
+
+class userClient {
+    @prop()
+    firstName: string
+    @prop()
+    lastName: string
+    @prop()
+    email: string
+    @prop()
+    password: string
+    @prop()
+    birthDate: string
+    @prop()
+    country: string
+}
+
+const userClientModel = getModelForClass(userClient)
+export default userClientModel
