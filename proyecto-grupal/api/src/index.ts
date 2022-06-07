652d88233212fe9286f990329a0ecbb01aaa38ec
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-07 16:02:55 -0500

    index change

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -1,41 +1,7 @@
 import mongoose from 'mongoose'
-import userClientModel from './models/userClients'
-import userPsychologistModel from './models/userPsychologist'
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
    console.log('database is connected to', db.connection.db.databaseName)
 }
 
 connectDB()
-
-/* async function testQuery(){
-   const testpaymentHistory = new paymentHistoryModel({
-      status: 'payed',
-      price: 1500,
-      type: 'debit card',
-      client: '629eb20ef872d3554abc739c',
-      psychologist: '629f99e7de87372b776f79ed'
-   })
-   await testpaymentHistory.save()
-}
-
-testQuery() */
-
-// async function testQuery(){
-//    const testUser =  new userPsychologistModel({
-//       firstName: 'Saul',
-//          lastName: 'Goodman',
-//          email: 'Saul2@gmail.com',
-//          password: 'asasd654ad',
-//          birthDate: '15/04/89',
-//          country: 'US',
-//          License: 'a544sda65s4654asd54a',
-//          DNI: 'asdasssdasd456sa54d',
-//          Specialties: ['family', 'couple'],
-//          profileImage: './images/holaa.png',
-//          rating: 5
-//    })
-//    await testUser.save()
-// } 
-
-// testQuery()
