a044f71c9e3c2bd4e347c7c8b6c24e2aa962459c
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-07 10:58:22 -0500

    change database mongo

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -3,22 +3,22 @@ import mongoose from 'mongoose'
 import userClientModel from './models/userClients'
 //test
 async function connectDB() {
-   const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/?retryWrites=true&w=majority')
+   const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
    console.log('database is connected to', db.connection.db.databaseName)
 }
 
 connectDB()
 
-async function testQuery(){
-   const testUser =  new userClientModel({
-      firstName: 'Saul',
-         lastName: 'Goodman',
-         email: 'Saul@gmail.com',
-         password: 'asasdad',
-         birthDate: '15/04/89',
-         country: 'US'
-   })
-   await testUser.save()
-} 
+// async function testQuery(){
+//    const testUser =  new userClientModel({
+//       firstName: 'Saul',
+//          lastName: 'Goodman',
+//          email: 'Saul@gmail.com',
+//          password: 'asasdad',
+//          birthDate: '15/04/89',
+//          country: 'US'
+//    })
+//    await testUser.save()
+// } 
 
-testQuery()
+// testQuery()
