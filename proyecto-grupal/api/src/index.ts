14139d3193585fb3389c8e7409b335789a43baea
Isabel Gestoso <92653093+isagestoso@users.noreply.github.com>
2022-06-07 17:22:31 -0300

    Update index.ts

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -2,7 +2,7 @@ import { getDiscriminatorModelForClass } from '@typegoose/typegoose'
 import mongoose from 'mongoose'
 import userClientModel from './models/userClients'
 import postModel from './models/Post'
-//testasasaas
+
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
    console.log('database is connected to', db.connection.db.databaseName)
@@ -22,18 +22,3 @@ async function testeQuery(){
    console.log(post)
 }
 testeQuery()
-
-// async function testQuery(){
-//    const testUser =  new userClientModel({
-//       firstName: 'Saul1',
-//          lastName: 'Goodman1',
-//          email: 'Saul1@gmail.com',
-//          password: 'asasdaddjis',
-//          birthDate: '15/04/89',
-//          country: 'US'
-//    })
-//    await testUser.save()
-//    console.log("se creo el usuario"+testUser)
-// } 
-
-// testQuery()
