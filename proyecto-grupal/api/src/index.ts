c1203a86469fd3dee9bad790e761c638a7e1e8e4
Isabel Gestoso <92653093+isagestoso@users.noreply.github.com>
2022-06-07 17:26:16 -0300

    Update index.ts

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-//testasasaas
+
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
    console.log('database is connected to', db.connection.db.databaseName)
@@ -7,44 +7,5 @@ async function connectDB() {
 
 connectDB()
 
-/* async function testQuery(){
-   const testpaymentHistory = new paymentHistoryModel({
-      status: 'payed',
-      price: 1500,
-      type: 'debit card',
-      client: '629eb20ef872d3554abc739c',
-      psychologist: '629f99e7de87372b776f79ed'
-   })
-   await testpaymentHistory.save()
-}
-
-testQuery() */
-
-// async function testQuery(){
-//    const testUser =  new userClientModel({
-//       firstName: 'Saul',
-//          lastName: 'Goodman',
-//          email: 'Saul@gmail.com',
-//          password: 'asasdad',
-//          birthDate: '15/04/89',
-//          country: 'US'
-//    })
-//    await testUser.save()
-// } 
-
-// testQuery()
-
-
-
-// async function Reviews(){
-//    const testReviews =  new reviewsModel({
-//       Content: 'articulo',
-//       Stars: 5
-//    })
-//    await testReviews.save()
-//    console.log(testReviews)
-// } 
-
-// Reviews()
 
 
