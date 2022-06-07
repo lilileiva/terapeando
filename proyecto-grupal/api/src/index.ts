014dbecd95d94288d144bfcb6d3cd93e37424c95
JeferProgramer <jefer.talento.11@gmail>
2022-06-07 15:02:10 -0500

    model post con relacion a el psicologo

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -1,6 +1,7 @@
 import { getDiscriminatorModelForClass } from '@typegoose/typegoose'
 import mongoose from 'mongoose'
 import userClientModel from './models/userClients'
+import postModel from './models/Post'
 //testasasaas
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
@@ -8,17 +9,31 @@ async function connectDB() {
 }
 
 connectDB()
+async function testeQuery(){
+   const post = new postModel({
+      Date:"2022-03-13",
+      Title:"Transtorno Depresivo",
+      Content:"este es el cotenido",
+      Image:"esta es la imagen",
+      Tags: ["depresion","cronica"],
+      idUserPsychologist:"629f99e7de87372b776f79ed",
+   })
+   await post.save()
+   console.log(post)
+}
+testeQuery()
 
 // async function testQuery(){
 //    const testUser =  new userClientModel({
-//       firstName: 'Saul',
-//          lastName: 'Goodman',
-//          email: 'Saul@gmail.com',
-//          password: 'asasdad',
+//       firstName: 'Saul1',
+//          lastName: 'Goodman1',
+//          email: 'Saul1@gmail.com',
+//          password: 'asasdaddjis',
 //          birthDate: '15/04/89',
 //          country: 'US'
 //    })
 //    await testUser.save()
+//    console.log("se creo el usuario"+testUser)
 // } 
 
 // testQuery()
