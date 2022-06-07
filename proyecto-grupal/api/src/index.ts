107fab25c6250c0a6bb5111c2c31dba2ccaafbb5
Esteban Longo <longoesteban@hotmail.com>
2022-06-07 19:58:40 -0300

    test

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -24,30 +24,29 @@ connectDB()
 }
 testQuery() */
 
-// async function testQuery(){
-//    const testUser =  new userClientModel({
-//       firstName: 'Saul',
-//          lastName: 'Goodman',
-//          email: 'Saul@gmail.com',
-//          password: 'asasdad',
-//          birthDate: '15/04/89',
-//          country: 'US'
-//    })
-//    await testUser.save()
-// } 
-
-// testQuery()
-
-async function testQuery(){
-   const testAppointment = new appointmentModel({
-      client: '629eb20ef872d3554abc739c',
-      psychologist: '629f97f5c767d20e0acad126',
-      payment: '629f9ce6ee9896aabc5fbcd2',
-      date: '07-06-2022',
-      hour: '18:00',
-      type: 'presencial'
+async function testQuer(){
+   const testUser =  new userClientModel({
+      firstName: 'Walter',
+         lastName: 'White',
+         email: 'heisenberg@gmail.com',
+         password: '1234',
+         birthDate: '15/04/89',
+         country: 'US'
    })
-   await testAppointment.save()
-}
-testQuery()
+   await testUser.save()
+} 
 
+testQuer()
+
+// async function testQuery(){
+//    const testAppointment = new appointmentModel({
+//       client: '629eb20ef872d3554abc739c',
+//       psychologist: '629f97f5c767d20e0acad126',
+//       payment: '629f9ce6ee9896aabc5fbcd2',
+//       date: '07-06-2022',
+//       hour: '18:00',
+//       type: 'presencial'
+//    })
+//    await testAppointment.save()
+// }
+// testQuery()
