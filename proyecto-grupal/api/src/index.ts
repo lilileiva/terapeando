a34dddac78a70bd2e7e0f1b72ee5b51d73bef3a9
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-07 13:45:26 -0500

    model psychologist

--- proyecto-grupal/api/src/index.ts
+++ proyecto-grupal/api/src/index.ts
@@ -1,7 +1,7 @@
 import { getDiscriminatorModelForClass } from '@typegoose/typegoose'
 import mongoose from 'mongoose'
 import userClientModel from './models/userClients'
-//testasasaas
+import userPsychologistModel from './models/userPsychologist'
 async function connectDB() {
    const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
    console.log('database is connected to', db.connection.db.databaseName)
@@ -10,13 +10,18 @@ async function connectDB() {
 connectDB()
 
 // async function testQuery(){
-//    const testUser =  new userClientModel({
+//    const testUser =  new userPsychologistModel({
 //       firstName: 'Saul',
 //          lastName: 'Goodman',
-//          email: 'Saul@gmail.com',
-//          password: 'asasdad',
+//          email: 'Saul2@gmail.com',
+//          password: 'asasd654ad',
 //          birthDate: '15/04/89',
-//          country: 'US'
+//          country: 'US',
+//          License: 'a544sda65s4654asd54a',
+//          DNI: 'asdasssdasd456sa54d',
+//          Specialties: ['family', 'couple'],
+//          profileImage: './images/holaa.png',
+//          rating: 5
 //    })
 //    await testUser.save()
 // } 
