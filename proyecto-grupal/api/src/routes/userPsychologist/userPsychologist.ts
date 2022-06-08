6a7276246557934ef22c4eda6743dfb34a7d081d
sergio17 <sergiosalgado624@gmail.com>
2022-06-08 18:07:13 -0500

    components

--- proyecto-grupal/api/src/routes/userPsychologist/userPsychologist.ts
+++ proyecto-grupal/api/src/routes/userPsychologist/userPsychologist.ts
@@ -1,7 +1,4 @@
 export {};
-const { Router } = require("express");
-import { Request, Response } from "express";
-const router = Router();
 import userPsychologistModel from "../../models/userPsychologist";
 import {Request, Response } from 'express';
 
@@ -54,52 +51,14 @@ const postUserPsychologist =  async (req: Request, res: Response) => {
       appointments:[]
     });
 
-
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      birthDay,
-      country,
-      license,
-      DNI,
-      specialities,
-      profileImage,
-      scheduleld,
-    } = req.body;
-
-    const userP = new userPsychologistModel({
-      firstName,
-      lastName,
-      email,
-      password,
-      birthDay,
-      country,
-      license,
-      DNI,
-      specialities,
-      profileImage,
-      scheduleld,
-    });
-
     await userP.save();
     res.status(201).send(userP);
   } catch (error) {
     res.status(404).send(error);
   }
 };
-module.exports = {
-    getUserPsychologistOne,
-    getUserPsychologist,
-    postUserPsychologist
 
- }
-});
-
-router.delete("/", async (req: Request, res: Response) => {
+ const deleteUserPsychologist =  async (req: Request, res: Response) => {
   const { idPsychologist } = req.params;
   try {
     await userPsychologistModel.findOneAndDelete({ idPsychologist });
@@ -107,4 +66,12 @@ router.delete("/", async (req: Request, res: Response) => {
   } catch (error) {
     res.status(404).send(error);
   }
-});
+};
+
+module.exports = {
+    getUserPsychologistOne,
+    getUserPsychologist,
+    postUserPsychologist,
+    deleteUserPsychologist
+
+ }
