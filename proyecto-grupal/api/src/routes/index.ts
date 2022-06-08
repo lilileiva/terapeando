37b4f0f11e32d4adfc1fdce06a0facb7f2e1055d
isagestoso <migdqmc@gmail.com>
2022-06-08 10:22:30 -0300

    agregue ruta post schedule

--- proyecto-grupal/api/src/routes/index.ts
+++ proyecto-grupal/api/src/routes/index.ts
@@ -2,9 +2,13 @@ import { Router} from "express";
 
 const {createUserClient, deletUserClient} = require('./userClient/userClientRoute')
 
+const {createSchedule} = require('./schedule/scheduleRoute')
+
 const router: Router = Router();
 
 router.post('/userclient', createUserClient)
 router.delete('/userclient/:IdUserClient', deletUserClient)
+
+router.post('/schedule', createSchedule)
 
 module.exports = router;
