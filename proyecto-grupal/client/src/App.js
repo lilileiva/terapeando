f800eb217f7ae7fae33bcb4fa71b140fcf5f7220
Esteban Longo <longoesteban@hotmail.com>
2022-06-08 16:32:33 -0300

    test navbar

--- proyecto-grupal/client/src/App.js
+++ proyecto-grupal/client/src/App.js
@@ -1,21 +1,13 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" component={NavBar} />
+      </Routes>
     </div>
   );
 }
