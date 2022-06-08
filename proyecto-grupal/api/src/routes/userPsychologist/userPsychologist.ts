a15337674e442346064d43cfa84e5be621cfa4b0
Esteban Longo <longoesteban@hotmail.com>
2022-06-08 12:48:32 -0300

    add route deletePsychologist

--- proyecto-grupal/api/src/routes/userPsychologist/userPsychologist.ts
+++ proyecto-grupal/api/src/routes/userPsychologist/userPsychologist.ts
@@ -1,41 +1,52 @@
 export {};
-const { Router} = require('express');
+const { Router } = require("express");
+import { Request, Response } from "express";
 const router = Router();
 import userPsychologistModel from "../../models/userPsychologist";
 
-// router.post("/", async (req: Request, res: Response) => {
-//   try {
-//     const {
-//       firstName: 
-//       lastName,
-//       email,
-//       password,
-//       birthDay,
-//       country,
-//       license,
-//       DNI,
-//       specialities,
-//       profileImage,
-//       scheduleld,
-//     } = req.body;
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      birthDay,
+      country,
+      license,
+      DNI,
+      specialities,
+      profileImage,
+      scheduleld,
+    } = req.body;
 
-//     const userP = new userPsychologistModel({
-//       firstName,
-//       lastName,
-//       email,
-//       password,
-//       birthDay,
-//       country,
-//       license,
-//       DNI,
-//       specialities,
-//       profileImage,
-//       scheduleld,
-//     });
+    const userP = new userPsychologistModel({
+      firstName,
+      lastName,
+      email,
+      password,
+      birthDay,
+      country,
+      license,
+      DNI,
+      specialities,
+      profileImage,
+      scheduleld,
+    });
 
-//     await userP.save();
-//     res.status(201).send(userP);
-//   } catch (error) {
-//     res.status(404).send(error);
-//   }
-// });
+    await userP.save();
+    res.status(201).send(userP);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.delete("/", async (req: Request, res: Response) => {
+  const { idPsychologist } = req.params;
+  try {
+    await userPsychologistModel.findOneAndDelete({ idPsychologist });
+    res.send("User deleted succesfully");
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
