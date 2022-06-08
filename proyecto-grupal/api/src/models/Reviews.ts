53131b96ca80b25a360dc55918e8ea52b8eb7a62
sergio17 <sergiosalgado624@gmail.com>
2022-06-07 23:53:08 -0500

    server escuchando y rutas get y pos reviews

--- proyecto-grupal/api/src/models/Reviews.ts
+++ proyecto-grupal/api/src/models/Reviews.ts
@@ -3,14 +3,14 @@ import { userClient } from './userClients';
 import { userPsychologist } from './userPsychologist'
 
 
-export class Reviews {
-    @prop({ required: true })
-    Content: string
-    @prop({ required: true, min: 1, max: 5 })
+export class Reviews{
+    @prop({required: true})  
+    Content: string 
+    @prop({required: true, min: 1, max: 5})  
     Stars: number
-    @prop({ ref: () => userClient })
+    @prop({ref: () => userClient})
     IdUserClient: Ref<userClient>
-    @prop({ ref: () => userPsychologist })
+    @prop({ref: () => userPsychologist})
     IdUserPsychologist: Ref<userPsychologist>
 }
 
