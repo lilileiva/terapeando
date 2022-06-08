988a5dd37747283ad7190cc9de1d2bf6dfe6f1ba
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-08 12:14:53 -0500

    firs commit client

--- proyecto-grupal/client/src/index.jsx
+++ proyecto-grupal/client/src/index.jsx
@@ -6,14 +6,16 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from "./redux/store/index.ts";
-
+import {chakraProvider} from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
+        <chakraProvider>
         <App />
+        </chakraProvider>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
