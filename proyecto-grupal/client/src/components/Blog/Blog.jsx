225d565942158820ce55f244bd1e91b1af9b9385
Esteban Longo <longoesteban@hotmail.com>
2022-06-09 18:15:00 -0300

    un poquito de logica al search

--- proyecto-grupal/client/src/components/Blog/Blog.jsx
+++ proyecto-grupal/client/src/components/Blog/Blog.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
-import Post from '../Post/Posts.jsx'
-import NavBar from '../NavBar/NavBar.jsx'
-import SearchBar from '../SearchBar/SearchBar.jsx'
-import './blog.css'
+import React from "react";
+import Post from "../Post/Posts.jsx";
+import NavBar from "../NavBar/NavBar.jsx";
+import SearchBar from "../SearchBar/SearchBar.jsx";
+import "./blog.css";
 
-export default function Blog () {
-
-    return(
-        <div>
-            <NavBar />
-            <SearchBar />
-            <h1 className='title'>Notas sobre psicología</h1>
-            <Post />
-
-            <h2>Secciones</h2>
-            <section>
-                <li>
-                    
-                </li>
-            </section>
-        </div>
-    )
+export default function Blog() {
+  return (
+    <div>
+      <NavBar />
+      <SearchBar />
+      <h1 className="title">Notas sobre psicología</h1>
+      <Post />
+    </div>
+  );
 }
--- proyecto-grupal/client/src/components/SearchBar/SearchBar.jsx
+++ proyecto-grupal/client/src/components/SearchBar/SearchBar.jsx
@@ -3,14 +3,32 @@ import "./searchbar.css";
 import { Input } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getAllPosts } from "../../redux/actions";
 
 export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // if (name.length === 0) alert("Type name of psychologist");
+    // if (isNaN(name) !== true) alert("Psychologist name cannot be a number");
+    dispatch(getAllPosts(name)); //name es el estado local
+    setName("");
+  }
+
   return (
     <div className="searchbar">
-      <Input />
+      <Input onChange={(e) => handleInputChange(e)} />
       <ButtonGroup variant="outline">
-        <Button 
-        widht={40}>
+        <Button widht={40} onClick={(e) => handleSubmit(e)}>
           <SearchIcon />
         </Button>
       </ButtonGroup>
