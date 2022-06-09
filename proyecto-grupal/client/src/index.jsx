94b59ca0b1a07c276fe65d866f1e5f8369509568
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-08 19:15:45 -0500

    Chakra tag added

--- proyecto-grupal/client/src/index.jsx
+++ proyecto-grupal/client/src/index.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./redux/store/index.ts";
-import {chakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <chakraProvider>
-        <App />
-        </chakraProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
