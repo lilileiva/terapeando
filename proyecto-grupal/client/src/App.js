2749a636e89ae4ec8edb85890282b0e65269f5ec
Esteban Longo <longoesteban@hotmail.com>
2022-06-09 15:03:13 -0300

    blog horrible

--- proyecto-grupal/client/src/App.js
+++ proyecto-grupal/client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
-import RegisterForm from './components/RegisterForm/RegisterForm';
 import Post from './components/Post/Posts.jsx'
-import CardPsychologist from './components/CardPsychologist/CardPsychologist';
+import CardPsychologist from './components/CardPsychologist/CardPsychologist.jsx';
 import RegisterForm from "./components/RegisterForm/RegisterForm.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Blog from "./components/Blog/Blog.jsx";
 
 function App() {
   return (
@@ -16,8 +16,9 @@ function App() {
         <Route path="/" element={< NavBar />} />
         <Route exact path='/signup' element={<RegisterForm />} />
         <Route exact path='/post' element={<Post />} />
-      <Footer />
+        <Route exact path='/blog' element={<Blog />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
