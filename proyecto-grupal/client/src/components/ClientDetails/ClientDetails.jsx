1c702a5c1718e9c059d33002cdf26ffcecb698c3
isagestoso <migdqmc@gmail.com>
2022-06-09 13:36:26 -0300

    User Client Detail component

--- proyecto-grupal/client/src/components/ClientDetails/ClientDetails.jsx
+++ proyecto-grupal/client/src/components/ClientDetails/ClientDetails.jsx
@@ -1,21 +1,83 @@
-import { Avatar, Container, Text, Box} from '@chakra-ui/react'
-import React from 'react'
+import {
+  Heading,
+  Avatar,
+  Box,
+  Center,
+  Text,
+  Stack,
+  Button,
+  Badge,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-function ClientDetails() {
+export default function ClientDetails() {
   return (
-    <div className='content'>
-       <Container d="flex" alignItems="center" py="10" >
-          <Avatar src='https://wallpaperaccess.com/full/4595683.jpg' size='2xl' mb='-40px' />
-          <Box bg="#E2E8F0" py="12"
-          borderRadius="8px" lineHeight={8}>
-          <Text fontSize='2xl'>Nombre y Apellido</Text>
-          <Text fontSize='md'>BirdthDay</Text>
-          <Text fontSize='md'>Country</Text>
-          <Text fontSize='md' variant="link">Email</Text>
-          </Box>
-       </Container>
-    </div>
-  )
-}
+    <Center py={6} h={'100%'}>
+      <Box
+        w={'80%'}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        p={6}
+        textAlign={'center'}>
+        <Avatar
+          size={'xl'}
+          src='https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+          alt={'Avatar Alt'}
+          mb={4}
+        />
+        <Heading fontSize={'2xl'} fontFamily={'body'}>
+          firstName lastName
+        </Heading>
 
-export default ClientDetails
+        <Text fontWeight={600} color={'gray.500'} mb={4}>
+        Información Personal
+        </Text>
+
+        <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
+          <Badge
+            px={2}
+            py={1}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+            fontWeight={'600'}>
+            email
+          </Badge>
+          <Badge
+            px={2}
+            py={1}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+            fontWeight={'600'}>
+            birthDate
+          </Badge>
+          <Badge
+            px={2}
+            py={1}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+            fontWeight={'600'}>
+            country
+          </Badge>
+        </Stack>
+
+        <Stack mt={8} direction={'row'} spacing={4}>
+          <Button
+            maxW={'40%'}
+            fontSize={'sm'}
+            rounded={'full'}
+            bg={'green.400'}
+            color={'white'}
+            boxShadow={
+              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+            }
+            _hover={{
+              bg: 'green.300',
+            }}
+            _focus={{
+              bg: 'teal.600',
+            }}>
+            Edit Profile
+          </Button>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
