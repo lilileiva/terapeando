6c55be53fa15370f4312f4b5a859630e033bb894
isagestoso <migdqmc@gmail.com>
2022-06-09 13:38:26 -0300

    agregue loguito y puse nuestro nombre en vez de chakra

---
+++ proyecto-grupal/client/src/components/Footer/Footer.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import img from '../../assets/logo-01.png'
+import {
+  Box,
+  chakra,
+  Container,
+  Image,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function SmallWithLogoLeft() {
+  return (
+    <Box
+      position={"fixed"}
+      left={0}
+      bottom={0}
+      width= '100%' 
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Image src={img} w={'50px'}/>
+        <Text>© 2022 Portal Psico. All rights reserved</Text>
+        <Stack direction={"row"} spacing={6}>
+          <SocialButton label={"Twitter"} href={"#"}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={"YouTube"} href={"#"}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={"Instagram"} href={"#"}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
