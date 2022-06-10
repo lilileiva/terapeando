1dd35ac122eca1ef74529c938969583c3d3f8169
sergio17 <sergiosalgado624@gmail.com>
2022-06-10 14:49:58 -0500

    algunos cambios cards psi

--- proyecto-grupal/api/src/models/userPsychologist.ts
+++ proyecto-grupal/api/src/models/userPsychologist.ts
@@ -1,4 +1,4 @@
-import {prop, getModelForClass, Ref, pre} from '@typegoose/typegoose'
+import { prop, getModelForClass, Ref, pre } from '@typegoose/typegoose'
 import * as mongoose from 'mongoose';
 import { appointment } from './appointment';
 import { Schedule } from './Schedule';
@@ -6,95 +6,95 @@ const bcrypt = require('bcryptjs');
 
 
 const saltRounds = 10;
-@pre<userPsychologist>('save', function(next) {
-    if (this.isModified('password')) {        
-        bcrypt.hash(this.password, saltRounds, (err: any, hashedPassword: any) => {
-            if (err) next(err);
-            else this.password = hashedPassword;
-            next();
-        } )
-    } else {
-        next();
-    }
+@pre<userPsychologist>('save', function (next) {
+  if (this.isModified('password')) {
+    bcrypt.hash(this.password, saltRounds, (err: any, hashedPassword: any) => {
+      if (err) next(err);
+      else this.password = hashedPassword;
+      next();
+    })
+  } else {
+    next();
+  }
 })
 
 export class userPsychologist {
-    @prop({
-        required: true,
-        trim: true,
-        validate: {
-          validator: (firstName: string) => {
-            const nameRegExp = /^[A-Za-z]+$/;
-            return nameRegExp.test(firstName);
-          },
-          message: 'Name is invalid.',
-        }
-    })
-    firstName: string
+  @prop({
+    required: true,
+    trim: true,
+    validate: {
+      validator: (firstName: string) => {
+        const nameRegExp = /^[A-Za-z]+$/;
+        return nameRegExp.test(firstName);
+      },
+      message: 'Name is invalid.',
+    }
+  })
+  firstName: string
 
-    @prop({
-        required: true,
-        trim: true,
-        validate: {
-          validator: (lastName: string) => {
-            const nameRegExp = /^[A-Za-z]+$/;
-            return nameRegExp.test(lastName);
-          },
-          message: 'Name is invalid.',
-        }
-    })
-    lastName: string
+  @prop({
+    required: true,
+    trim: true,
+    validate: {
+      validator: (lastName: string) => {
+        const nameRegExp = /^[A-Za-z]+$/;
+        return nameRegExp.test(lastName);
+      },
+      message: 'Name is invalid.',
+    }
+  })
+  lastName: string
 
-    @prop({
-        unique: true,
-        required: true,
-        lowercase: true,
-        trim: true,
-        validate: {
-          validator: (email: string) => {
-            const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return emailRegExp.test(email);
-          },
-          message: 'Email is invalid.',
-        }
-    })
-    email: string
+  @prop({
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: (email: string) => {
+        const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegExp.test(email);
+      },
+      message: 'Email is invalid.',
+    }
+  })
+  email: string
 
-    @prop({ unique: false ,required: true, minlength:8 })
-    password: string
+  @prop({ unique: false, required: true, minlength: 8 })
+  password: string
 
-    @prop({ required: true})
-    birthDate: string
+  @prop({ required: true })
+  birthDate: string
 
-    @prop({  required: true })
-    country: string
+  @prop({ required: true })
+  country: string
 
-    @prop({ unique: true, required: true })
-    License: string
+  @prop({ unique: true, required: true })
+  License: string
 
-    @prop()
-    about: string
+  @prop()
+  about: string
 
-    @prop()
-    education: string
+  @prop()
+  education: string
 
-    @prop({ unique: true, required: true })
-    DNI: string
+  @prop({ unique: true, required: true })
+  DNI: string
 
-    @prop({ required: true })
-    Specialties: string[]
+  @prop({ required: true })
+  Specialties: string[]
 
-    @prop({required: true })
-    profileImage: string
+  @prop({ required: true })
+  profileImage: string
 
-    @prop()
-    rating: number
+  @prop()
+  rating: number
 
-    @prop({ ref: () => appointment })
-    appointments?: Ref<appointment>[]; 
+  @prop({ ref: () => appointment })
+  appointments?: Ref<appointment>[];
 
-    // @prop({ ref: () => Schedule })
-    // schedule?: Ref<Schedule>[]; 
+  // @prop({ ref: () => Schedule })
+  // schedule?: Ref<Schedule>[]; 
 }
 
 const userPsychologistModel = getModelForClass(userPsychologist)
