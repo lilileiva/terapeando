d8c30e74f50c389947be5e62766bed2c8973d67e
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-11 20:27:19 -0500

    Psychologist Detail previus page button added and profile image changed place

--- proyecto-grupal/client/src/components/PsychologistDetail/PsychologistDetail.jsx
+++ proyecto-grupal/client/src/components/PsychologistDetail/PsychologistDetail.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Box, SimpleGrid, Heading, Badge } from "@chakra-ui/react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { clear, getUserPsychologistOne } from "../../redux/actions";
 import img from '../../assets/logo-01.png'
 import './PsychologistDetail.css'
 import Starts from '../Starts/Starts';
+import { ArrowLeftIcon, CheckIcon } from '@chakra-ui/icons';
+
 export default function PsychologistDetail() {
   const dispatch = useDispatch();
   const { IdUserPsychologist} = useParams();
   const detail = useSelector((state) => state.userPsichologistDetail);
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getUserPsychologistOne(IdUserPsychologist));
     return () => {
@@ -25,40 +27,44 @@ export default function PsychologistDetail() {
     backgroundPosition='top'
     backgroundSize={'cover'}
     backgroundRepeat="no-repeat"  columns={3} spacingX="40px" spacingY="20px">
-        <Box className="BoxDetail" marginBottom={'15px'} bg="white" height="120px" width='120px' zIndex='2'>
-        <img src={detail.profileImage} alt="" width='120rem' height='4rem'/>
-        </Box>
-        <Box className="BoxDetail"><Heading mb={3}>Conoce un poco más sobre tu próximo psicólogo</Heading></Box>
+        
+        <Link to={'/psicologos'}>
+       <ArrowLeftIcon color={'green.300'} alignItems={'left'} onClick={() => navigate(-1)}/>
+       </Link>
+        <Box className="BoxDetail"><Heading className="HeadingDetail" mb={3}>Conoce un poco más sobre tu próximo psicólogo</Heading></Box>
         <Box className="BoxDetail">    <img className="imageDetailLogo" src={img} alt="" width={'60rem'} /> </Box>
       </SimpleGrid>
       <SimpleGrid  columns={1} marginTop={'20'} textAlign={'left'} paddingLeft={'32'} spacingX="40px" spacingY="20px">
+      <Box className="BoxDetail" marginBottom={'50px'} bg="" height="120px" width='120px'>
+        <img src={detail.profileImage} alt="" width='120rem' height='4rem'/>
+        </Box>
         <Box className="BoxDetail" bg="" borderRadius={'10px'}  height="80px" zIndex='2'> 
-          <Heading as='h3' size='lg'>
+          <Heading className="HeadingDetail" as='h3' size='lg'>
             {`${detail.firstName} ${detail.lastName}`}
           </Heading> 
         </Box>
       <Box className="BoxDetail" bg=""  borderRadius={'10px'} height="80px">
-      <Heading as='h4' size='md'>
+      <Heading className="HeadingDetail" as='h4' size='md'>
         {` 🎂 ${detail.birthDate}`} 
       </Heading> 
-      <Heading as='h4' size='md' >
+      <Heading className="HeadingDetail" as='h4' size='md' >
       {`📍${detail.country}`}
       </Heading>
       </Box>
 
       <Box className="BoxDetail" bg="" borderRadius={'10px'} height="80px">
-      <Heading as='h4' size='md'> 
+      <Heading className="HeadingDetail" as='h4' size='md'> 
         {`📩${detail.email}`}
       </Heading> 
         </Box>
 
       <Box className="BoxDetail" bg="" borderRadius={'10px'} height="80px">
-      <Heading as='h4' size='md'> 
+      <Heading className="HeadingDetail" as='h4' size='md'> 
         {`🎓${detail.education}, Licencia: ${detail.License}`}
       </Heading> 
         </Box>
         <Box className="BoxDetail" bg=""  borderRadius={'10px'}   height="200px">
-      <Heading as='h4' size='md'> 
+      <Heading className="HeadingDetail" as='h4' size='md'> 
         
         Especialidades: {detail.Specialties && detail.Specialties.map((e)=> <ul> <Badge variant='subtle' colorScheme='purple'>{`${e}`}</Badge></ul> )        
         }
@@ -66,13 +72,13 @@ export default function PsychologistDetail() {
         </Box>
         </SimpleGrid>
       <Box className="BoxDetail" bg="" borderRadius={'10px'} height="fit-content">
-      <Heading as='h5' size='md'> 
+      <Heading className="HeadingDetail" as='h5' size='md'> 
       <h3>Sobre mí</h3>
         {`${detail.about}`}
       </Heading> 
         </Box>
       <Box className="BoxDetail" bg="" borderRadius={'10px'} height="80px">
-      <Heading as='h4' size='md'> 
+      <Heading className="HeadingDetail" as='h4' size='md'> 
         Mi calificación promedio 😊: <Starts
                 rating={detail.rating}/>
       </Heading> 
