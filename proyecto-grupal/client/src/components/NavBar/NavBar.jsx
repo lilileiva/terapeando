c00dd727bd079e28a3fbcb13b0831796af27637a
Esteban Longo <longoesteban@hotmail.com>
2022-06-13 12:58:30 -0300

    agregando boton home a nav y link en 404notFound

--- proyecto-grupal/client/src/components/NavBar/NavBar.jsx
+++ proyecto-grupal/client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Box,
   Flex,
@@ -15,78 +15,81 @@ import {
   useColorModeValue,
   Image,
   useDisclosure,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   HamburgerIcon,
   CloseIcon,
   ChevronDownIcon,
   ChevronRightIcon,
-} from '@chakra-ui/icons';
-import img from '../../assets/logo-01.png'
-import './navbar.css';
+} from "@chakra-ui/icons";
+import img from "../../assets/logo-01.png";
+import "./navbar.css";
 
 export default function WithSubnavigation() {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box position='relative'>
+    <Box position="relative">
       <Flex
         // bg={useColorModeValue('white', 'gray.800')}
-        color={useColorModeValue('gray.600', 'white')}
-        minH={'60px'}
+        color={useColorModeValue("gray.600", "white")}
+        minH={"60px"}
         py={{ base: 2 }}
         px={{ base: 4 }}
         // borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align={'center'}>
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
         <Flex
-          flex={{ base: 1, md: 'auto' }}
+          flex={{ base: 1, md: "auto" }}
           ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}>
+          display={{ base: "flex", md: "none" }}
+        >
           <IconButton
             onClick={onToggle}
             icon={
               isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
             }
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
           />
         </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Image src={img} alt='portal psico' w={'50px'} />
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Image src={img} alt="portal psico" w={"50px"} />
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
             <DesktopNav />
           </Flex>
         </Flex>
 
         <Stack
           flex={{ base: 1, md: 0 }}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}>
-
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
           <Button
-            as={'a'}
-            fontSize={'sm'}
+            as={"a"}
+            fontSize={"sm"}
             fontWeight={400}
-            variant={'link'}
-            href={'/signin'}
+            variant={"link"}
+            href={"/signin"}
           >
             Sign In
           </Button>
 
-          <Link to='/signup'>
+          <Link to="/signup">
             <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
+              display={{ base: "none", md: "inline-flex" }}
+              fontSize={"sm"}
               fontWeight={600}
-              color={'white'}              
-              bg={'#63caa7'}
-              href={'/signup'}
+              color={"white"}
+              bg={"#63caa7"}
+              href={"/signup"}
               _hover={{
-                bg: '#6bdfb7'
-              }}>
+                bg: "#6bdfb7",
+              }}
+            >
               Sign Up
             </Button>
           </Link>
@@ -100,28 +103,36 @@ export default function WithSubnavigation() {
   );
 }
 
-
 const DesktopNav = () => {
   // const linkColor = useColorModeValue('gray.600', 'gray.200');
   // const linkHoverColor = useColorModeValue('gray.800', 'white');
   // const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
-
   return (
-    <div className={'contenedorlinks'}>
-      <Link className={'links'} to={"/preguntasfrecuentes"} >
-        <Button bg='white' height='1.5em' _hover={{bg: '#cbd5e0bb'}} >Preguntas Frecuentes</Button>
+    <div className={"contenedorlinks"}>
+      <Link className={"links"} to={"/home"}>
+        <Text fontWeight={"500"} color="gray.600">
+          Home
+        </Text>
       </Link>
-      <Link className={'links'} to={"/psicologos"} >
-        <Button bg='white' height='1.5em' _hover={{bg: '#cbd5e0bb'}} >Psicologos</Button>
+      <Link className={"links"} to={"/preguntasfrecuentes"}>
+        <Button bg="white" height="1.5em" _hover={{ bg: "#cbd5e0bb" }}>
+          Preguntas Frecuentes
+        </Button>
       </Link>
-      <Link className={'links'} to={"/blog"} >
-        <Button bg='white' height='1.5em' _hover={{bg: '#cbd5e0bb'}} >Blog</Button>
+      <Link className={"links"} to={"/psicologos"}>
+        <Button bg="white" height="1.5em" _hover={{ bg: "#cbd5e0bb" }}>
+          Psicologos
+        </Button>
+      </Link>
+      <Link className={"links"} to={"/blog"}>
+        <Button bg="white" height="1.5em" _hover={{ bg: "#cbd5e0bb" }}>
+          Blog
+        </Button>
       </Link>
       {/* <Link className={'links'} to={"/psicologos"}>Preguntas Frecuentes</Link>
       <Link className={'links'} to={"/psicologos"}>Psicologos</Link>
       <Link className={'links'} to={"/blog"}>Blog</Link> */}
-
     </div>
     // <Stack direction={'row'} spacing={4}>
     //   {NAV_ITEMS.map((navItem) => (
@@ -171,38 +182,36 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
   return (
     <Stack
       href={href}
-      role={'group'}
-      display={'block'}
+      role={"group"}
+      display={"block"}
       p={2}
-      rounded={'md'}
-      _hover={{ bg: useColorModeValue('green.50', 'gray.900') }}>
-      <Stack direction={'row'} align={'center'}>
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("green.50", "gray.900") }}
+    >
+      <Stack direction={"row"} align={"center"}>
         <Box>
-
-
-
           <Link to={`${href}`}>
             <Text
-              transition={'all .3s ease'}
-              _groupHover={{ color: 'green.300' }}
-              fontWeight={500}>
+              transition={"all .3s ease"}
+              _groupHover={{ color: "green.300" }}
+              fontWeight={500}
+            >
               {label}
             </Text>
           </Link>
 
-
-
-          <Text fontSize={'sm'}>{subLabel}</Text>
+          <Text fontSize={"sm"}>{subLabel}</Text>
         </Box>
         <Flex
-          transition={'all .3s ease'}
-          transform={'translateX(-10px)'}
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
           opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify={'flex-end'}
-          align={'center'}
-          flex={1}>
-          <Icon color={'green.300'} w={5} h={5} as={ChevronRightIcon} />
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
+          flex={1}
+        >
+          <Icon color={"green.300"} w={5} h={5} as={ChevronRightIcon} />
         </Flex>
       </Stack>
     </Stack>
@@ -212,9 +221,10 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
 const MobileNav = () => {
   return (
     <Stack
-      bg={useColorModeValue('white', 'gray.800')}
+      bg={useColorModeValue("white", "gray.800")}
       p={4}
-      display={{ md: 'none' }}>
+      display={{ md: "none" }}
+    >
       {NAV_ITEMS.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
@@ -229,36 +239,39 @@ const MobileNavItem = ({ label, children, href }) => {
     <Stack spacing={4} onClick={children && onToggle}>
       <Flex
         py={2}
-        href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
+        href={href ?? "#"}
+        justify={"space-between"}
+        align={"center"}
         _hover={{
-          textDecoration: 'none',
-        }}>
+          textDecoration: "none",
+        }}
+      >
         <Text
           fontWeight={600}
-          color={useColorModeValue('gray.600', 'gray.200')}>
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
           {label}
         </Text>
         {children && (
           <Icon
             as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
             w={6}
             h={6}
           />
         )}
       </Flex>
 
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
         <Stack
           mt={2}
           pl={4}
           borderLeft={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.700')}
-          align={'start'}>
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
           {children &&
             children.map((child) => (
               <Text key={child.label} py={2} href={child.href}>
@@ -273,25 +286,25 @@ const MobileNavItem = ({ label, children, href }) => {
 
 const NAV_ITEMS = [
   {
-    label: 'Preguntas Frecuentes',
-    href: 'preguntasfrecuentes',
+    label: "Preguntas Frecuentes",
+    href: "preguntasfrecuentes",
     children: [
       {
-        label: 'Precios',
-        href: 'precios',
+        label: "Precios",
+        href: "precios",
       },
       {
-        label: 'Términos y Condiciones',
-        href: 'terminosycondiciones',
+        label: "Términos y Condiciones",
+        href: "terminosycondiciones",
       },
     ],
   },
   {
-    label: 'Psicologos',
-    href: 'psicologos',
+    label: "Psicologos",
+    href: "psicologos",
   },
   {
-    label: 'Blog',
-    href: 'blog',
+    label: "Blog",
+    href: "blog",
   },
 ];
