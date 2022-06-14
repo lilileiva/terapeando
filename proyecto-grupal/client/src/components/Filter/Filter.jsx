41ad45b512d5a5a54aecd4caf99a61790bf718e8
JeferProgramer <jefer.talento.11@gmail>
2022-06-14 08:13:18 -0500

    modificando importaciones

--- proyecto-grupal/client/src/components/Filter/Filter.jsx
+++ proyecto-grupal/client/src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect}from "react";
 import './filter.css'
 import { useDispatch, useSelector } from "react-redux";
-import { getPostOrder,getCategories,getByCategory } from "../../../redux/actions";
+import { getPostOrder,getCategories,getByCategory } from "../../redux/actions";
 import { Select } from "@chakra-ui/react";
 
 
