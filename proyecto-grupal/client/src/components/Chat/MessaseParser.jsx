19f4765e3b2d1f658be679454342be93b67b412b
JeferProgramer <jefer.talento.11@gmail>
2022-06-21 16:49:15 -0500

    preparand al bot para responder lo que escriba el usuario con preguntas comunes

--- proyecto-grupal/client/src/components/Chat/MessaseParser.jsx
+++ proyecto-grupal/client/src/components/Chat/MessaseParser.jsx
@@ -9,7 +9,26 @@ class MessageParser{
         if(lowerCaseMessage.includes("hola")){
             this.actionProvider.greet();
         }
-        
+        if(lowerCaseMessage.includes("menu") || lowerCaseMessage.includes("volver")){
+            this.actionProvider.handleMenu(9)
+        }
+        if(lowerCaseMessage.includes("contactanos")){
+            this.actionProvider.handleHire();
+        }
+        if(lowerCaseMessage.includes("cool") || lowerCaseMessage.includes("awesome") || 
+        lowerCaseMessage.includes("amazing")){
+            this.actionProvider.handleCool();
+        }
+        if (lowerCaseMessage.includes("amo")) {
+            
+        }
+        if (lowerCaseMessage.includes("precio") ||
+        lowerCaseMessage.includes("valor") || lowerCaseMessage.includes("cuanto cuesta")) {
+            this.actionProvider.handlePricing();
+        }
+        if (lowerCaseMessage.includes("ok") || lowerCaseMessage.incluide("okey") || lowerCaseMessage.includes("vale") || lowerCaseMessage.includes("gracias")) {
+            this.actionProvider.handleSomethingElse()
+        } 
     }
 }
 export default MessageParser
