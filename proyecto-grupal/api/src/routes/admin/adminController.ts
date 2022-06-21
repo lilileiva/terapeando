2cf1f24af5d76d19b9c27ba001f8f8d4f7609f93
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-21 14:06:29 -0500

    fixed see psychologist details

--- proyecto-grupal/api/src/routes/admin/adminController.ts
+++ proyecto-grupal/api/src/routes/admin/adminController.ts
@@ -127,8 +127,8 @@ const getAllUserPsychologist = async (req: Request, res: Response, next: NextFun
 
 const getPsychologistDetail = async (req: Request, res: Response) => {
   try {
-    const { IdUserPsychologist } = req.params;
-    const psychologistUser = await userPsychologistModel.findById(IdUserPsychologist, '-password');
+    const { idUserPsychologist } = req.params;
+    const psychologistUser = await userPsychologistModel.findById(idUserPsychologist, '-password');
     res.status(200).json(psychologistUser)
   } catch (err) {
     res.status(404).json({ data: err })
