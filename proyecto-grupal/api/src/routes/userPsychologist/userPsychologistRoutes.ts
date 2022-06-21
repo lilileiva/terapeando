efd5ad037557ca45526a821e58a02e07c644772c
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-06-21 11:37:44 -0500

    fixing bugs psychologist detail as psycologist

--- proyecto-grupal/api/src/routes/userPsychologist/userPsychologistRoutes.ts
+++ proyecto-grupal/api/src/routes/userPsychologist/userPsychologistRoutes.ts
@@ -21,7 +21,7 @@ const psychologistRouter: Router = Router();
 
 // psychologistRouter.get('/', validatePychologist , getUserPsychologistOne); aca sería validación para inicio de sesión
 psychologistRouter.get('/profile', validatePsychologist, getUserPsychologistOne);
-psychologistRouter.get('/:IdUserPsychologist', validateClient ,getPsychologistDetails)
+psychologistRouter.get('/:idUserPsychologist', validatePsychologist ,getPsychologistDetails)
 psychologistRouter.get('/', getUserPsychologist);
 psychologistRouter.get('/status/psycologiststatus', getUserPsychologistByStatus); //Uso admin
 psychologistRouter.post('/', postUserPsychologist); //registro
