02b93b8aa6837ea53ce7379cde24f48b14c2da7f
Liliana Leiva <liliana-carmen@hotmail.com>
2022-06-22 15:38:22 -0300

    button volver a market en map

--- proyecto-grupal/client/src/components/Map/Map.jsx
+++ proyecto-grupal/client/src/components/Map/Map.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { BsGeoAltFill } from "react-icons/bs";
+import { Stack } from '@chakra-ui/react';
 
 const { REACT_APP_GOOGLE_MAP_API_KEY } = process.env;
 
@@ -20,25 +22,32 @@ function Map() {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map)
-  }, [])  
+  }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
   }, [])
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={{ width: '25em', height: '25em' }}
-      center={center}
-      zoom={20}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-      options={{ gestureHandling: "greedy" }}
-    >
-      { /* Child components, such as markers, info windows, etc. */}
-      <Marker position={center} title='Tu psicólogo' />
-      
-    </GoogleMap>
+    <Stack direction='column' align='center'>
+
+      <GoogleMap
+        mapContainerStyle={{ width: '25em', height: '25em' }}
+        center={center}
+        zoom={20}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{ gestureHandling: "greedy" }}
+      >
+        { /* Child components, such as markers, info windows, etc. */}
+        <Marker position={center} title='Tu psicólogo' />
+      </GoogleMap>
+
+      <button onClick={() => map.panTo(center)}>
+        <BsGeoAltFill width='1.5em' color='red' />
+      </button>
+
+    </Stack>
   ) : <></>
 }
 
