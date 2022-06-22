a58c3d13539cf185d5e25a256a0a429994560c4d
JeferProgramer <jefer.talento.11@gmail>
2022-06-22 11:34:30 -0500

    agregando mas parseos

--- proyecto-grupal/client/src/components/Chat/MessageParser/MessageParser.jsx
+++ proyecto-grupal/client/src/components/Chat/MessageParser/MessageParser.jsx
@@ -13,35 +13,38 @@ class MessageParser{
         if(lowerCaseMessage.includes("menu") || lowerCaseMessage.includes("volver")){
             this.actionProvider.handleMenu()
         }
-        // if(lowerCaseMessage.includes("contactanos")){
-        //     this.actionProvider.handleHire();
-        // }
-        // if(lowerCaseMessage.includes("cool") || lowerCaseMessage.includes("awesome") || 
-        // lowerCaseMessage.includes("amazing")){
-        //     this.actionProvider.handleCool();
-        // }
-        // if (lowerCaseMessage.includes("amo")) {
-            
-        // }
-        // if (lowerCaseMessage.includes("precio") ||
-        // lowerCaseMessage.includes("valor") || lowerCaseMessage.includes("cuanto cuesta")) {
-        //     this.actionProvider.handlePricing();
-        // }
-        // if (lowerCaseMessage.includes("ok")|| lowerCaseMessage.includes("okey") || lowerCaseMessage.includes("vale") || lowerCaseMessage.includes("gracias")) {
-        //     this.actionProvider.handleSomethingElse()
-        // } 
-        // if(lowerCaseMessage.includes("contratar") || lowerCaseMessage.incluides("trabajar para ti")){
-        //     this.actionProvider.handleHire();
-        // }
-        // if(lowerCaseMessage.includes("servicios")){
-        //     this.actionProvider.handleServicesList();
-        // }
-        // if(lowerCaseMessage.includes("blog")){
-        //     this.actionProvider.handleBlog();
-        // }
-        // if(lowerCaseMessage.includes("cita")){
-        //     this.actionProvider.handleSitePack1();
-        // }
+        if(lowerCaseMessage.includes("contactanos")){
+            this.actionProvider.handleHire();
+        }
+        if(lowerCaseMessage.includes("cool") || lowerCaseMessage.includes("impresionante") || 
+        lowerCaseMessage.includes("genial")){
+            this.actionProvider.handleCool();
+        }
+        if (lowerCaseMessage.includes("amo")) {
+            this.actionProvider.handleLove()
+        }
+        if (lowerCaseMessage.includes("precio") ||
+        lowerCaseMessage.includes("valor") || lowerCaseMessage.includes("cuanto")) {
+            this.actionProvider.handlePricing();
+        }
+        if (lowerCaseMessage.includes("ok")|| lowerCaseMessage.includes("okey") || lowerCaseMessage.includes("vale") || lowerCaseMessage.includes("gracias")) {
+            this.actionProvider.handleSomethingElse()
+        } 
+        if(lowerCaseMessage.includes("contratar") || lowerCaseMessage.includes("trabajar")){
+            this.actionProvider.handleHire();
+        }
+        if(lowerCaseMessage.includes("servicios")){
+            this.actionProvider.handleServicesList();
+        }
+        if(lowerCaseMessage.includes("blog")){
+            this.actionProvider.handleBlog();
+        }
+        if(lowerCaseMessage.includes("cita")){
+            this.actionProvider.handleSitePack1();
+        }
+        if(lowerCaseMessage.includes("adios") || lowerCaseMessage.includes("bye")){
+            this.actionProvider.handleGoodbye();
+        }
     }
 }
 export default MessageParser
