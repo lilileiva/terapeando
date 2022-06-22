b2c13c31377a46e42cdc8b1cf2dfb2dd02c027e8
JeferProgramer <jefer.talento.11@gmail>
2022-06-22 09:23:59 -0500

    poniendo el widget de servicios

--- proyecto-grupal/client/src/components/Chat/Chat.jsx
+++ proyecto-grupal/client/src/components/Chat/Chat.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import Chatbot from 'react-chatbot-kit'
+import {Chatbot} from 'react-chatbot-kit'
 import "./Chat.css"
 import config from './config/config.jsx';
 import ActionProvider from './ActionProvider/ActionProvider.jsx';
 import MessageParser from "./MessageParser/MessageParser.jsx"
+import 'react-chatbot-kit/build/main.css';
 function Chat(){
     return(
-        <div className='Chat'>
-            <header className='chat-header'>
+        <div className='App'>
+            <header className='App-header'>
                 <Chatbot
-                config={config}
-                actionProvider={ActionProvider}
-                messageParser={MessageParser}
+                    config={config}
+                    actionProvider={ActionProvider}
+                    messageParser={MessageParser}
                 />
             </header>
         </div>
