a694a72352ad23cc7a810a84b32f106960d7ebac
JeferProgramer <jefer.talento.11@gmail>
2022-06-23 17:33:50 -0500

    comoponenete creado para el avatar

---
+++ proyecto-grupal/client/src/components/Chat/Components/Avatar/Avatar.jsx
@@ -0,0 +1,9 @@
+//me traigo nuestr logo
+import avatar from "../../../../assets/logo-01.png"
+
+const Avatar = () => {
+    <div>
+        <img style={{width:30, height:30, marginRight:13, marginTop:3}} src={avatar} alt="logo" />
+    </div>
+}
+export default Avatar
