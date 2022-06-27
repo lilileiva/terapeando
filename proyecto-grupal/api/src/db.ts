6dbd5980f8bb315d7e8b0184d523b58a3b02989a
JeferProgramer <jefer.talento.11@gmail>
2022-06-27 18:52:09 -0500

    arreglando la db para errores

--- proyecto-grupal/api/src/db.ts
+++ proyecto-grupal/api/src/db.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
 require('dotenv').config();
-const { DB_NAME, DB_PASSWORD, DB_USERNAME, DB_CLUSTERNAME } = process.env
+const {DB_NAME, DB_PASSWORD, DB_USERNAME, DB_CLUSTERNAME} = process.env
 
 
 // Database Connection
 export default async function connectDB() {
-   try {
-      const db = await mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTERNAME}.z5mrv.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+   try{
+      // const db = await mongoose.connect('mongodb+srv://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal.z5mrv.mongodb.net/ProyectoGrupaltest?retryWrites=true&w=majority')
+      const db = await mongoose.connect('mongodb://proyectogrupal:VNWSkd5ixj7hLVTo@proyectogrupal-shard-00-00.z5mrv.mongodb.net:27017,proyectogrupal-shard-00-01.z5mrv.mongodb.net:27017,proyectogrupal-shard-00-02.z5mrv.mongodb.net:27017/ProyectoGrupaltest?ssl=true&replicaSet=atlas-xq3ym6-shard-0&authSource=admin&retryWrites=true&w=majority')
       console.log('database is connected to', db.connection.db.databaseName)
-   } catch (e) {
+   }catch(e){
       console.log(e)
    }
 }
