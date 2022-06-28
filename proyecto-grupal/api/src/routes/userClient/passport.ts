d81139a8146f6ce4f9071005a72d1b621124818c
JeferProgramer <jefer.talento.11@gmail>
2022-06-28 16:56:20 -0500

    templete de html completo si no se registra primero

--- proyecto-grupal/api/src/routes/userClient/passport.ts
+++ proyecto-grupal/api/src/routes/userClient/passport.ts
@@ -26,7 +26,7 @@ module.exports = function (passport:any) {
             done(null, user)
           } else {
             // if user is not preset in our database save user data to database.
-            done("<h1 style='color:green'>primero registrate</h1> <a href='http://localhost:3000/signin'>Volver a iniciar sesion</a>",false)
+            done("<div style='height:100%;background: #e0e5ec;'><h1 style='padding-top: 10%; position: relative;text-align: center;color: #353535;font-size: 50px;font-family: verdana;'>Primero registrate</h1><p style='font-family:verdana;font-weight: 300;text-align: center;font-size: 18px;color: #676767;'>antes de logearte con google primero debes registrarte en nuestra plataforma para poder logearte a terapeando desde google, da click en el boton registrate que te redirecciona a registrarte en la plataforma o vuelve a intentarlo en el boton Inicia sesión </p> <div style=' width: 90%;margin: 40px auto;text-align: center;'><button style='width: 25%;height: 40px;color: #fff;border-radius: 5px;padding: 10px 25px;font-family: verdana;font-weight: 500;background: transparent;cursor: pointer;transition: all 0.3s ease;position: relative;display: inline-block; box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);outline: none; background: rgb(6,14,131);background: linear-gradient(0deg, rgb(18 211 77) 0%, rgb(12 180 25) 100%);border: none;'><a href='http://localhost:3000/signin' style='text-decoration:none; color:whitesmoke;'><span>Inicia sesión</span></a></button><button style='width: 25%;height: 40px;color: #fff;border-radius: 5px;padding: 10px 25px;font-family: verdana;font-weight: 500;background: transparent;cursor: pointer;transition: all 0.3s ease;position: relative;display: inline-block; box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);outline: none; background: rgb(6,14,131);background: linear-gradient(0deg, rgb(18 211 77) 0%, rgb(12 180 25) 100%);border: none;'><a href='http://localhost:3000/signup' style='text-decoration:none; color:whitesmoke;'><span>Registrate</span></a></button></div>",false)
           }
         } catch (err) {
           console.error(err)
@@ -46,5 +46,3 @@ module.exports = function (passport:any) {
     userClientModel.findById(id, (err:any, user:any) => done(err, user))
   })
 }
-
-
