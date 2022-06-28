43d823e2643c157c61e98ebc5f9f6070f7b43dda
JeferProgramer <jefer.talento.11@gmail>
2022-06-28 14:46:31 -0500

    arreglando la ruta

--- proyecto-grupal/api/src/routes/userClient/userClientRoutes.ts
+++ proyecto-grupal/api/src/routes/userClient/userClientRoutes.ts
@@ -11,27 +11,18 @@ const {
 const validateClient = require('../../middleware/validateClient')
 const validateAdmin = require('../../middleware/ValidateAdminToken')
 import { Request, Response } from "express";
-import userClientModel from "../../models/userClients";
 const passport = require('passport')
-<<<<<<< HEAD
-const clientRouter:Router= Router();
-=======
 import userClientModel from "../../models/userClients";
 import userPsychologistModel from "../../models/userPsychologist";
 const clientRouter: Router = Router();
->>>>>>> c49881fc30ba21556bfb58d99e0027acefe32a19
 const jwt = require("jsonwebtoken");
 
 
 
 clientRouter.get('/auth/google/callback', passport.authenticate('google'), async(req: any, res: Response) => {
+    console.log(req.user)
     if (req.user) { 
-<<<<<<< HEAD
-        const user = await userClientModel.findOne({email: req.user.email });
-=======
-
         const user = req.user.role === 'client' ?  await userClientModel.findOne({email: req.user.email }) : await userPsychologistModel.findOne({email: req.user.email })
->>>>>>> c49881fc30ba21556bfb58d99e0027acefe32a19
         const userForToken = {
             id: user?._id,
             role: user?.role
@@ -39,22 +30,12 @@ clientRouter.get('/auth/google/callback', passport.authenticate('google'), async
       const token = jwt.sign(userForToken, process.env.SECRETWORD, {
         expiresIn: 60 * 60 * 24 // equivalente a 24 horas
       })
-<<<<<<< HEAD
       res.redirect(`http://localhost:3000/home?token=${token}`)
     } else {
         res.redirect('http//localhost:3000/signin')
     } 
 })
-clientRouter.get('/google', passport.authenticate('google', { scope: ['profile','email']}))
-=======
-      res.redirect(`http://localhost:3000/home?role=${req.user.role}&token=${token}`)
-    } else {
-        res.redirect('http//localhost:3000/signin')
-    } 
-  })
-  
 clientRouter.get('/auth/google', passport.authenticate('google', { scope: ['profile','email']}))
->>>>>>> c49881fc30ba21556bfb58d99e0027acefe32a19
 clientRouter.get('/client',validateClient, getUserClient);
 clientRouter.get('/:IdUserPsychologist', validateClient ,getPsychologistDetails)
 clientRouter.post('/client/register', createUserClient)
